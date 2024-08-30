2a1285f603ca2a62a1ccbcb0a72a3f9b9dfeccf1
AmnaFaisal <76680437+AmnaFaisal@users.noreply.github.com>
2024-08-30 18:29:54 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
-window.addEventListener("scroll", function() {
-    var navbar = document.querySelector(".navbar");
-    if (window.scrollY > 50) { // Adjust the scroll position as needed
-        navbar.classList.add("scrolled");
+window.addEventListener('scroll', function() {
+    var navbar = document.querySelector('.navbar');
+    var menuIcon = document.querySelector('.menu-option');
+    var logoImage = document.querySelector('.logo img');
+    
+    
+    var scrollThreshold = 425; 
+    
+    if (window.scrollY > scrollThreshold) {
+        navbar.classList.add('scrolled');
+        menuIcon.style.opacity = '1';
+        logoImage.src = "images/golden_logo.png"; 
     } else {
-        navbar.classList.remove("scrolled");
+        navbar.classList.remove('scrolled');
+        menuIcon.style.opacity = '0';
+        logoImage.src = "images/logo.png";
     }
 });
+
+
 
      function countUp(elementId, target, duration) {
             let start = 0;
@@ -59,7 +71,7 @@ window.addEventListener("scroll", function() {
         var menuIcon = document.querySelector('.menu-icon svg');
         var logoImage = document.querySelector('.logo img');
 
-        if (scrollPercentage > 5) {
+        if (scrollPercentage > 13) {
             navbar.style.backgroundColor = '#ffffff';
             navbar.style.borderBottomRightRadius = '20%';
             navbar.style.boxShadow = '0 0 20px rgba(0, 0, 0, 0.3)';
@@ -82,3 +94,21 @@ window.addEventListener("scroll", function() {
             var overlay = document.getElementById('overlay');
             overlay.style.display = 'none';
         }
+
+        function scrollToRegistration() {
+            const registrationSection = document.querySelector('.registration');
+            const offset = -150; 
+        
+            const topPosition = registrationSection.getBoundingClientRect().top + window.pageYOffset + offset;
+            window.scrollTo({ top: topPosition, behavior: 'smooth' });
+        }
+
+        function scrollToFooter() {
+            const registrationSection = document.querySelector('.footer');
+            const offset = 0; 
+        
+            const topPosition = registrationSection.getBoundingClientRect().top + window.pageYOffset + offset;
+            window.scrollTo({ top: topPosition, behavior: 'smooth' });
+        }
+        
+       
